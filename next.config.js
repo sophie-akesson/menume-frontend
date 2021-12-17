c3a04bdb98a4474c298fe4d21f9495255b347153
Sophie Åkesson <sophie.akesson@osynlig.se>
2021-12-17 18:26:19 +0100

    Add global styling

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
